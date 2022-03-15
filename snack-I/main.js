74082aa315f5875a5986e817154e77e2b51d3704
giagra91 <giagra91@gmail.com>
2022-03-15 08:42:45 +0100

    modifiche snack I

--- snack-I/main.js
+++ snack-I/main.js
@@ -1,11 +1,19 @@
 const mainArray = [1, 2, 3, 4 , 5, 6, 7];
 
-let firstNumber = Math.floor(Math.random()* mainArray.length);
-let secondNumber = Math.floor(Math.random() * firstNumber);
+let firstNumber = randomInt(0, mainArray.length -1);
+let secondNumber = randomInt(0, firstNumber - 1);
 console.log(firstNumber, secondNumber);
 
 function createNewArray (arrayOne, numOne, numTwo){
-    return arrayOne.slice(numTwo,numOne);
+    const newArray= [];
+    for (let i = numTwo ; i <= numOne ; i++){
+        newArray.push(arrayOne[i]);
+    }
+    return newArray;
 }
 
 console.table(createNewArray(mainArray, firstNumber,secondNumber));
+
+function randomInt (min,max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
